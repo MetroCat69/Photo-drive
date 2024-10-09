86beebdc27917d84df7ad3bef94a0ba71a3c3c87
MetroCat69 <aaaaa>
2024-10-09 13:45:57 +0300

    refactor to have index

--- back/src/index.ts
+++ back/src/index.ts
@@ -1,14 +1,13 @@
 import express from "express";
 
 const app = express();
-
 app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Hello, World!");
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
