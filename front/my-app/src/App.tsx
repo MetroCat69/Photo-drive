822b5c9585d5cf0eeb1d5493562abf27f4768682
MetroCat69 <aaaaa>
2024-10-11 19:16:21 +0300

    aaa

--- front/my-app/src/App.tsx
+++ front/my-app/src/App.tsx
@@ -1,23 +1,68 @@
 import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
-import MyButton from "./Button"; // Adjust the path as neededfunction App() {
-export function App() {
+import {
+  AppBar,
+  Box,
+  Drawer,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemText,
+  Toolbar,
+  Typography,
+} from "@mui/material";
+
+const drawerWidth = 240;
+
+function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        {MyButton}
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Box sx={{ display: "flex" }}>
+      <AppBar position="fixed">
+        <Toolbar>
+          {/* <Typography variant="h1" noWrap component="div">
+            My Drive
+          </Typography> */}
+        </Toolbar>
+      </AppBar>
+
+      {/* <Drawer
+        sx={{
+          width: drawerWidth,
+          flexShrink: 0,
+          "& .MuiDrawer-paper": {
+            width: drawerWidth,
+            boxSizing: "border-box",
+          },
+        }}
+        variant="permanent"
+        anchor="left"
+      >
+        <Toolbar />
+        <List>
+          {["My Drive", "Starred", "Bin"].map((text) => (
+            <ListItemButton key={text}>
+              <ListItemText primary={text} />
+            </ListItemButton>
+          ))}
+        </List>
+      </Drawer>
+
+      <Box
+        component="main"
+        sx={{ flexGrow: 1, bgcolor: "background.default", p: 3 }}
+      >
+        <Toolbar />
+        <Typography variant="h4" gutterBottom>
+          Folders
+        </Typography>
+        <Box>
+          <List>
+            <ListItemButton>
+              <ListItemText primary="Folder" />
+            </ListItemButton>
+          </List>
+        </Box>
+      </Box> */}
+    </Box>
   );
 }
 
