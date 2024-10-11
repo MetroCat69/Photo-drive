dc063fc3c68fa97a92681a4822c8f35583758b34
MetroCat69 <aaaaa>
2024-10-11 21:46:43 +0300

    staged

--- back/src/types/user.ts
+++
@@ -1,7 +0,0 @@
-export interface User {
-  email: string;
-  username: string;
-  userId: string;
-  bucketId: string;
-  creationTime: Date;
-}
--- front/my-app/src/Button.tsx
+++
@@ -1,4 +0,0 @@
-import * as React from "react";
-import Button from "@mui/material/Button";
-const but = <Button variant="contained">Hello world</Button>;
-export default but;
